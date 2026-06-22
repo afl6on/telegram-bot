d90db6ecd45cf86d226f0a3329eccf68f307fa53
7kem <8893738+afl6on@users.noreply.github.com>
2026-06-22 10:05:57 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+import telebot
+import os
+
+TOKEN = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "مرحباً! أنا بوتك 🤖")
+
+@bot.message_handler(func=lambda m: True)
+def echo(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
